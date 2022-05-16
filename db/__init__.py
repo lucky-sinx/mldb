662835d97c0f29f21cda086b6c2a4320e896fe31
wangyu <2549261744@qq.com>
2022-05-16 14:21:03 +0800

    peewee

--- db/__init__.py
+++ db/__init__.py
@@ -1,11 +1,53 @@
-import pymysql
+import datetime
+from peewee import *
 
-conn = pymysql.connect(
-    host='127.0.0.1',  # 连接名称，默认127.0.0.1
-    user='root',  # 用户名
-    passwd='root',  # 密码
-    port=3306,  # 端口，默认为3306
-    db='mltest',  # 数据库名称
-    charset='utf8',  # 字符编码
-)
-conn.connect()
+db = MySQLDatabase('mltest', host='127.0.0.1', user='root', passwd='root', charset='utf8', port=3306)
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db  # 将实体与数据库进行绑定
+
+
+class DataSet(BaseModel):
+    id = AutoField()
+    dataset_name = CharField(100)
+    dataset_description = CharField(100)
+    path = CharField(100)
+    create_time = DateTimeField()
+
+    @staticmethod
+    def add_dataset(name, path, description=""):
+        DataSet.create(dataset_name=name, path=path, dataset_description=description,
+                       create_time=datetime.datetime.now())
+
+
+class Model(BaseModel):
+    id = AutoField()
+    model_name = CharField(100)
+    model_type = CharField(100)
+    model_path = CharField(200)
+    create_time = DateTimeField()
+    algorithm_param = CharField(200)
+
+    @staticmethod
+    def add_model(model_name, model_type, model_path, algorithm_param):
+        Model.create(model_name=model_name, model_path=model_path, model_type=model_type,
+                     create_time=datetime.datetime.now(), algorithm_param=algorithm_param)
+
+
+class Task(BaseModel):
+    id = AutoField()
+    task_name = CharField(100)
+    task_description = CharField(100)
+    path = CharField(100)
+    create_time = DateTimeField()
+
+
+class TaskLog(BaseModel):
+    id = AutoField()
+    task_id = BigIntegerField()
+    generate_model_id = BigIntegerField()
+    result = CharField(100)
+    start_time = DateTimeField()
+    end_time = DateTimeField()
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,13 +1,70 @@
+import datetime
+import json
+import os
 import unittest
+
+from peewee import AutoField
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
 
-    def test_connect(self):
-        from db import dbUtils
-        dbUtils.show_tables()
+    def test_peewee(self):
+        from peewee import MySQLDatabase, Model, CharField, DateField, BooleanField, IntegerField
+
+        # py_peewee连接的数据库名
+        db = MySQLDatabase('mltest', host='127.0.0.1', user='root', passwd='root', charset='utf8', port=3306)
+
+        class BaseModel(Model):
+            class Meta:
+                database = db  # 将实体与数据库进行绑定
+
+        class Person(BaseModel):  # 继承自BaseModel，直接关联db，并且也继承了Model。Model有提供增删查改的函数
+            id1 = AutoField()
+            name = CharField(verbose_name='姓名', max_length=10, null=False, index=True)
+            passwd = CharField(verbose_name='密码', max_length=20, null=False, default='111111')
+            gender = IntegerField(verbose_name='姓别', null=False, default=1)
+            is_admin = BooleanField(verbose_name='是否是管理员', default=False)
+
+        # 查询数据库是连接
+        print(db.is_closed())  # 返回false未连接
+        # 连接数据库
+        db.connect()
+        print(db.is_closed())  # 返回true表示已连接
+        # 创建table
+        Person.create_table(safe=False)
+
+    def test_dataset(self):
+        from db import DataSet
+        # DataSet1.create_table(safe=True)
+        DataSet.create(dataset_name="dataset1", dataset_description="test", path="./dataset",
+                       create_time=datetime.datetime.now())
+        DataSet.add_dataset("dataset2", "./dataset")
+        DataSet.delete().execute()
+
+    def test_model(self):
+        from db import Model
+        Model.add_model("model1", "classification", "./model", json.dumps({"test": 1}))
+
+    def test_task(self):
+        from db import Task
+        query = {
+            "task_name": "task1",
+            "task_description": "test",
+            "path": "F:\code\python\mldb\\temp\\task\\1.json",
+            "create_time": datetime.datetime.now()
+        }
+        # Task.delete().execute()
+        Task.create(**query)
+
+    def test_load_dataset(self):
+        from db import DataSet
+        data_path = "F:\code\python\mldb\\temp\dataset"
+        for file_name in os.listdir(data_path):
+            DataSet.add_dataset(file_name.split('.')[0], os.path.join(data_path, file_name),
+                                "test dataset:" + file_name)
+
 
 if __name__ == '__main__':
     unittest.main()
